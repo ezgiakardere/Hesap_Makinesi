bf0d0704685f95591f71dd3b8917bb03b4fe5dde
ezgiakardere <ezgiakardere35@gmail.com>
2023-07-11 16:02:12 +0300

    hesap makinesi switch-case olarak düzenlendi

--- Hesap_Makinesi/Program.cs
+++ Hesap_Makinesi/Program.cs
@@ -12,75 +12,87 @@ namespace Hesap_Makinesi
     {
         static void Main(string[] args)
         {
-        
-        int sayi1, sayi2;
 
-            double sonuc;
-            string islem;
-            FileStream fs = new FileStream("C:\\HesapMakinesi.txt", FileMode.Append, FileAccess.Write, FileShare.Write);
-            StreamWriter sw = new StreamWriter(fs);
-            DateTime bugun = DateTime.Now;
-            sw.WriteLine("İşlemi " + bugun + " tarihinde yaptın.");
-
-            Console.WriteLine("Birinci sayıyı giriniz:");
-            sayi1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("İkinci sayıyı giriniz:");
-            sayi2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Çarpma işlemi için 1\nToplama işlemi için 2\nÇıkarma işlemi için 3\n" +
-                "Bölme işlemi için 4");
-            islem = Console.ReadLine();
-            
-                if (islem == "1")
-                {
-                    sonuc = sayi1 * sayi2;
-                    sw.WriteLine("Çarpma işleminin sonucu:" + sonuc);
-                Console.WriteLine(sonuc);
-                    Console.ReadKey();
-                
-                sw.Close();
-                }
-                if (islem == "2")
-                {
-                    sonuc = sayi1 + sayi2;
-                    sw.WriteLine("Toplama işleminin sonucu:" + sonuc);
-                Console.WriteLine(sonuc);
-                Console.ReadKey();
-                sw.Close();
-                }
-                if (islem == "3")
-                {
-                    sonuc = sayi1 - sayi2;
-                    sw.WriteLine("Çıkarma işleminin sonucu:" + sonuc);
-                Console.WriteLine(sonuc);
-                Console.ReadKey();
-                sw.Close();
-                }
-            if (islem == "4")
+            while (true)
             {
-                if (sayi1 == 0 && sayi2 == 0)
-                {
-                    sw.WriteLine("Bölmenin sonucu :Belirsiz");
-                    Console.ReadKey();
-                    sw.Close();
-                }
-                else if (sayi2 == 0)
-                {
-                    sw.WriteLine(" Bölmenin sonucu:Tanımsız");
-                    Console.ReadKey();
-                    sw.Close();
-                }
-                else
-                {
-                    sonuc = sayi1 / sayi2;
-                    sw.WriteLine("Bölme işleminin sonucu:" + sonuc);
-                    Console.WriteLine(sonuc);
-                    Console.ReadKey();
-                    sw.Close();
-                }
-            }
-        }
+                int sayi1, sayi2;
 
+
+                FileStream fs = new FileStream("C:\\HesapMakinesi.txt", FileMode.Append, FileAccess.Write, FileShare.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                DateTime bugun = DateTime.Now;
+                sw.WriteLine("İşlemi " + bugun + " tarihinde yaptın.");
+
+                Console.WriteLine("Çarpma işlemi için 1\nToplama işlemi için 2\nÇıkarma işlemi için 3\n" +
+                  "Bölme işlemi için 4\nÇıkmak için -1 e basınız");
+                Console.Write("Seçim yapınız: ");
+               
+                double islem = Convert.ToDouble(Console.ReadLine());
+                if (islem == -1)
+                {
+                    Console.WriteLine("Çıkış yapılıyor");
+                    Console.ReadKey();
+                    break;
+                }
+                Console.WriteLine("Birinci sayıyı giriniz:");
+                sayi1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("İkinci sayıyı giriniz:");
+                sayi2 = Convert.ToInt32(Console.ReadLine());
+
+              
+              
+
+                switch (islem)
+                {
+                    case 1:
+                        sw.WriteLine("Çarpma işleminin sonucu=" + sayi1 + "*" + sayi2 + "=" + (sayi1 * sayi2));
+                        Console.WriteLine(sayi1 * sayi2);
+                        Console.ReadKey();
+
+                        sw.Close();
+                        break;
+                    case 2:
+                        sw.WriteLine("Toplama işleminin sonucu=" + sayi1 + "+" + sayi2 + "=" + (sayi1 + sayi2));
+                        Console.WriteLine(sayi1 + sayi2);
+                        Console.ReadKey();
+
+                        sw.Close();
+                        break;
+                    case 3:
+                        sw.WriteLine("Çıkarma işleminin sonucu=" + sayi1 + "-" + sayi2 + "=" + (sayi1 - sayi2));
+                        Console.WriteLine(sayi1 - sayi2);
+                        Console.ReadKey();
+
+                        sw.Close();
+                        break;
+                    case 4:
+                        sw.WriteLine("Bölme işleminin sonucu=" + sayi1 + "/" + sayi2 + "=" + (sayi1 / sayi2));
+                        Console.WriteLine(sayi1 / sayi2);
+                        Console.ReadKey();
+
+                        sw.Close();
+                        break;
+                    case 5:
+                        Console.WriteLine("Çıkış yapılıyor");
+                        Console.ReadKey();
+                        break;
+                  
+                    default:
+                        Console.WriteLine("Geçersiz işlem");
+                        break;
+
+                }
+
+                Console.WriteLine("Kayıt Eklendi.");
+                Console.ReadKey();
+                
+
+            }
+          
         }
+    }
 }
+
+       
+
